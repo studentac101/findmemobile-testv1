bde5601d5aeceb882d016abf5669150ea966518c
Unknown <studentac101@gmail.com>
2018-04-12 08:37:43 +0800

    v8

--- connection.php
+++ connection.php
@@ -8,12 +8,10 @@ $dbname = "pftkgwykbk";
 // Create connection
 $conn = new mysqli($servername, $username, $password,$dbname);
 
-var_dump($conn);
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
 
-http://phplaravel-166083-479984.cloudwaysapps.com/
-http://phpstack-166083-480072.cloudwaysapps.com/
