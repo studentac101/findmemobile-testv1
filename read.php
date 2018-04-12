d5f4226966f19833d37601891752b73cbb7074c1
Unknown <studentac101@gmail.com>
2018-04-12 09:34:02 +0800

    v13

--- read.php
+++ read.php
@@ -1,7 +1,7 @@
 <?php
-
+  phpinfo(); 
   require 'connection.php';
-
+	
     $sql = "SELECT b.id,a.first_name,a.last_name,a.gender,a.nationality,a.height,a.weight,a.medical_history,a.body_marks,a.top,a.bottom,a.imgurl,
 	a.date_last_seen FROM  missings as a JOIN incidents as b ON a.id=b.missing_id AND b.status=1";
     
