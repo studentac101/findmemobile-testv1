50978888328ea568e38d82aa11c28c056e849620
Unknown <studentac101@gmail.com>
2018-04-12 12:34:24 +0800

    v29

--- read.php
+++ read.php
@@ -5,20 +5,20 @@
     $sql = "SELECT b.id,a.first_name,a.last_name,a.gender,a.nationality,a.height,a.weight,a.medical_history,a.body_marks,a.top,a.bottom,a.imgurl,
 	a.date_last_seen FROM  missings as a JOIN incidents as b ON a.id=b.missing_id AND b.status=1";
     
-    $row=array();
-    $data=array();	
+    
      if ($result=$conn->query($sql)) {
 	
 		
 	if($result->{"num_rows"}!=0){
-	 while( $row[] = $result->fetch_assoc()){
+	 while( $row= $result->fetch_assoc()){
 
-        $data=utf8_encode($row);
+        $data=json_encode($row);
 		
 	 }
 	
 	
-	  echo json_encode($data);
+	  echo $data;
+	  echo '<br>';
 		 var_dump($row);
   
 
