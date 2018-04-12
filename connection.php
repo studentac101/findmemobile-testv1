2543c87ff01d6533aced2901a11a3395964449fd
Unknown <studentac101@gmail.com>
2018-04-12 13:02:49 +0800

    v32

--- connection.php
+++ connection.php
@@ -14,6 +14,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-mysqli_set_charset($conn,"utf8");
+
 ?>
 
--- read.php
+++ read.php
@@ -13,7 +13,7 @@
 	if($result->{"num_rows"}!=0){
 	 while( $row[] = $result->fetch_assoc()){
 
-        $data=json_encode($data);
+        $data=json_encode((object)$data);
 		
 	 }
 	
