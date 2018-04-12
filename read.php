07a0d988bbabb03acf2bc71a6529d5003b0ed0ee
Unknown <studentac101@gmail.com>
2018-04-12 12:14:26 +0800

    v25

--- read.php
+++ read.php
@@ -13,8 +13,8 @@
 	if($result->{"num_rows"}!=0){
 	 while( $row[] = $result->fetch_assoc())
 
-        $data=wp_json_encode($row);
-	  echo $data;
+        $data=utf8_encode($row);
+	  echo json_encode($data_;
 
   
 
