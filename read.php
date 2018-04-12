9b5e387e24cef359fd77c79c4ecf23ff73ba6b3a
Unknown <studentac101@gmail.com>
2018-04-12 08:56:45 +0800

    v9

--- read.php
+++ read.php
@@ -13,7 +13,13 @@
 	if($result->{"num_rows"}!=0){
 	 while( $row[] = $result->fetch_assoc())
         $data=json_encode($row);
+	
+	 return $data;
+	 echo '<br>';
 	  echo $data;
+	  	echo '<br>';
+	  var_dump($data);
+	echo '<br>';
 	}else{
 		echo "failed";
 	}
