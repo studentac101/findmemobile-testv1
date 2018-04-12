e7148dc049e55df8aa6931027cf6f3d2059f39cb
Unknown <studentac101@gmail.com>
2018-04-12 08:27:54 +0800

    v7

--- connection.php
+++ connection.php
@@ -1,8 +1,9 @@
 <?php
-$servername = "206.189.35.125:8082/";
+$servername = "206.189.35.125:8082";
 $username = "pftkgwykbk";
 $password = "PTmfa3paTg";
 $dbname = "pftkgwykbk";
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password,$dbname);
@@ -13,3 +14,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
+http://phplaravel-166083-479984.cloudwaysapps.com/
+http://phpstack-166083-480072.cloudwaysapps.com/
